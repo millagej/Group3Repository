319cf418161a90d2408bed38776f67e06e3855c6
millagej <46766359+millagej@users.noreply.github.com>
2020-04-29 17:44:06 -0400

    Update HelloGroup.java

--- src/HelloGroup.java
+++ src/HelloGroup.java
@@ -1,9 +1,8 @@
 public class HelloGroup {
     public static void main(String[] args) {
 
-        System.out.println("Hello Future Group-3 SDET");
-        
-        System.out.println("Hi. Everyone.This line is added by: Rahman Abdalla");
+        System.out.println("Hello Future SDET");
+        System.out.println("Hello, this is your fellow naighborhood developer, Joshua Millage");
 
     }
 }
