ba16017d702a4331914b2b6207de366c6df5b2a0
RahmanAbdalla <64114912+RahmanAbdalla@users.noreply.github.com>
2020-04-29 17:42:24 -0400

    Update HelloGroup.java

--- src/HelloGroup.java
+++ src/HelloGroup.java
@@ -1,7 +1,9 @@
 public class HelloGroup {
     public static void main(String[] args) {
 
-        System.out.println("Hello Future SDET");
+        System.out.println("Hello Future Group-3 SDET");
+        
+        System.out.println("Hi. Everyone.This line is added by: Rahman Abdalla");
 
     }
 }
